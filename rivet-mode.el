834f7569718a1639f0f0be08ee53b3b60e6c346b
Jade Michael Thornton <jade@jmthornton.net>
2019-10-24 16:32:23 -0500

    remove commented code
    we can look at it in git if we really need

--- rivet-mode.el
+++ rivet-mode.el
@@ -60,12 +60,7 @@
   (hack-local-variables)
 
   (if rivet-switch-hook
-      (run-hooks 'rivet-switch-hook))
-  ;; (if font-lock-mode (font-lock-ensure))
-  ;; (if (fboundp 'turn-on-font-lock-if-enabled)
-  ;;     (turn-on-font-lock-if-enabled)
-  ;;   (turn-on-font-lock-if-desired))
-  )
+      (run-hooks 'rivet-switch-hook)))
 
 (defun rivet-mode-maybe-change-mode (to-mode)
   "Change to TO-MODE if current mode is not TO-MODE."

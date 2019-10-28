d7c44f2782f4c34c5c71ec538629ec73dd7668d6
Jade Michael Thornton <jade@jmthornton.net>
2019-10-28 15:00:08 -0500

    prepare for tagged release

--- rivet-mode.el
+++ rivet-mode.el
@@ -4,7 +4,7 @@
 ;; Copyright (c) 2019 Jade Michael Thornton
 ;; Package-Requires: ((emacs "24") (web-mode "16"))
 ;; URL: https://gitlab.com/thornjad/rivet
-;; Version: 3.4.0
+;; Version: 4.0.0
 ;;
 ;; This file is not part of GNU Emacs
 ;;
